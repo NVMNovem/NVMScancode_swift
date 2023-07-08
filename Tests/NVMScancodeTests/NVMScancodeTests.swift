80854a27ba6e60d134b47569e7759b74cf21652b
Damian Van de Kauter <vdkdamian@gmail.com>
2023-07-08 08:21:58 +0200

    init

--- Tests/NVMScancodeTests/NVMScancodeTests.swift
+++ Tests/NVMScancodeTests/NVMScancodeTests.swift
@@ -3,9 +3,6 @@ import XCTest
 
 final class NVMScancodeTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(NVMScancode().text, "Hello, World!")
+        
     }
 }
